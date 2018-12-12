f709beb8ef9639010b2c721f8674c3ae5a8b30d0
Ilkka Prusi <ilkka.prusi@gmail.com>
2018-12-12 14:25:10 +0200

    license marking

--- src/liblinux.c
+++ src/liblinux.c
@@ -1,8 +1,15 @@
+/*
+ * liblinux.c 
+ * 3-clause BSD-license, see LICENSE for details
+ * 
+ * Ilkka Prusi <ilkka.prusi@gmail.com> 2018
+ */
 #include "liblinux.h"
 
 #include <linux/unistd.h>
 #include <sys/syscall.h>
 
+/* syscall number: 39 */
 long getpid()
 {
     return syscall(SYS_getpid);
--- src/liblinux.h
+++ src/liblinux.h
@@ -1,3 +1,10 @@
+/*
+ * liblinux.h 
+ * 3-clause BSD-license, see LICENSE for details
+ * 
+ * Ilkka Prusi <ilkka.prusi@gmail.com> 2018
+ */
+
 #ifndef _LIBLINUX_H_
 #define _LIBLINUX_H_
 
